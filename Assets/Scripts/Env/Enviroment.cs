400e38dd53665487d073954ddc4385995a975b14
kyumin <151908469+Chisato93@users.noreply.github.com>
2024-08-22 12:34:49 +0900

    CameraViewerSetting
    Change FOV for Mouse Wheel

---
+++ Assets/Scripts/Env/Enviroment.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : MonoBehaviour
+{
+    public List<GameObject> islandsList;
+}
---
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Camera cam;
+
+    const float fovmin = 50, fovmax = 120;
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll > 0f)
+        {
+            cam.fieldOfView++;
+            if (cam.fieldOfView >= fovmax)
+                cam.fieldOfView = fovmax;
+        }
+        else if (scroll < 0f)
+        {
+            cam.fieldOfView--;
+            if (cam.fieldOfView <= fovmin)
+                cam.fieldOfView = fovmin;
+        }
+    }
+}
